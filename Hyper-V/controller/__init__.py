70f55138659edcb7c2f313e5de7e4915aa2a6022
FreshBread <white_len@naver.com>
2016-11-30 12:30:44 +0900

    Rest 함수 컨트롤러 작성 시작

--- Hyper-V/controller/__init__.py
+++ Hyper-V/controller/__init__.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'jhjeon'
+
 from flask import Flask
+from controller.powershellController import *
+
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.add_url_rule("/vm", view_func=newVm, methods=['POST'])
 
 
 if __name__ == '__main__':
--- Hyper-V/controller/service/powershell.py
+++ Hyper-V/controller/service/powershell.py
@@ -7,6 +7,3 @@ class PowerShell():
     def __init__(self, address, port):
         self.address = address
         self.port = port
-
-    #파이선에서 rest api 접근하도록
-
