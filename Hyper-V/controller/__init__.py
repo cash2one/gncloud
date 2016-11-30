d721536c3f6f54e8ad18adf33f604f4416f5d93b
FreshBread <white_len@naver.com>
2016-11-30 18:37:56 +0900

    Hyper-V Controller DB 설정

--- Hyper-V/controller/__init__.py
+++ Hyper-V/controller/__init__.py
@@ -3,13 +3,15 @@ __author__ = 'jhjeon'
 
 from flask import Flask
 from controller.powershellController import *
+from db.models import *
 
 
 app = Flask(__name__)
 
 app.add_url_rule("/manual", view_func=manual, methods=['GET', 'POST'])
-app.add_url_rule("/vm/status/<vm_name>/<status>", view_func=hvm_state, methods=['GET', 'POST'])
+app.add_url_rule("/vm/status/<vm_name>/<status>", view_func=hvm_state, methods=['POST'])
 
 
 if __name__ == '__main__':
+    app.config['DEBUG'] = False
     app.run()
---
+++ Hyper-V/controller/util/config.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# 고정적으로 사용하는 값들을 한 곳에 모아두기 위해 만든 상수용 파이썬 파일
+import ConfigParser
+import os
+import sys
+
+
+class Config:
+
+    DB_URL = ""
+
+    def __init__(self, path="../conf/config.conf"):
+        # 외부 Config파일을 환경변수로 설정시 이 파일을 이용한다.
+        config_section = "DEFAULT"
+        config_file = os.environ.get('APP_CONFIG')
+        if config_file is None or config_file == '':
+            config_file = os.path.join(os.path.dirname(__file__), path)
+
+        if os.path.isfile(config_file):
+            self.load(config_file, config_section)
+        else:
+            sys.exit('Cannot read config file : ' + config_file)
+
+    def load(self, config_file, config_section="DEFAULT"):
+        parser = ConfigParser.ConfigParser()
+        result = parser.read(config_file)
+
+        self.DB_URL = parser.get(config_section, "DB_URL")
+
+
+# 전역 공통사용 객체이다.
+config = Config()
