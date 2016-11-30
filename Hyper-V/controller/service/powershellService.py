dc8262120a62c90c512c4ea544d3deaa081cbdd2
asdhgfasd <asdhgfasd@naver.com>
2016-11-30 13:52:35 +0900

    powershellService.py Powershell() 클래스 작성 send() 함수 정의

--- Hyper-V/controller/service/powershellService.py
+++ Hyper-V/controller/service/powershellService.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 __author__ = 'gncloud'
 
+import urllib, json
+import requests
 
 class PowerShell():
 
-    def __init__(self, address, port):
+    def __init__(self, address, port, uri):
         self.address = address
         self.port = port
+        self.uri = uri
 
     def send(self):
-        print "testsss"
+        address = self.address
+        port = str(self.port)
+        uri = self.uri
+        #URL = "http://60.196.149.135:8180/powershell/execute?script=Get-VM%20%7C%20Convertto-Json"
+        URL = address
+        URL += ":"
+        URL += port
+        URL += "/"
+        URL += uri
+
+        data = {'param1': 'value1', 'param2': 'value'}
+        response = requests.post(URL, data = json.dumps(data))
+        #print URL
+        print response.json()
