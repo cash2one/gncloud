ed2307aeae14c0ae7081ad10001204525a6dbe7d
FreshBread <white_len@naver.com>
2016-12-05 14:48:29 +0900

    Hyper-V controller angularJS 라이브러리 추가 및 테스트 페이지 설정을 kvm 쪽에서 가져옴 (추후 수정 필요)

--- Hyper-V/controller/__init__.py
+++ Hyper-V/controller/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 __author__ = 'jhjeon'
 
-from flask import Flask, redirect
+from flask import Flask, redirect, send_file
 from controller.powershellController import *
 from util.config import config
 
 
 app = Flask(__name__)
+
 # PowerShell Script Manual 실행: (Script) | ConvertTo-Json
 app.add_url_rule("/manual", view_func=manual, methods=['GET'])
 # VM 생성 및 실행
@@ -27,9 +28,40 @@ def isAlive():
     return jsonify(status=True, message='서비스 정상 작동')
 
 
+# 페이지 리스트
 @app.route("/")
 def index():
-    return redirect("/service/isAlive")
+    return send_file("static/html/index.html")
+
+
+@app.route("/main")
+def main():
+    return send_file("static/html/main.html")
+
+
+@app.route("/guestCreate")
+def guest_create():
+    return send_file("static/html/guestCreate.html")
+
+
+@app.route("/guestList")
+def guest_list():
+    return send_file("static/html/guestList.html")
+
+
+@app.route("/guestLogin")
+def guest_login():
+    return send_file("static/html/guestLogin.html")
+
+
+@app.route("/guestRunList")
+def guest_run_list():
+    return send_file("static/html/guestRunList.html")
+
+
+@app.route("/guestSnapList")
+def guest_snap_list():
+    return send_file("static/html/guestSnapList.html")
 
 
 if __name__ == '__main__':
