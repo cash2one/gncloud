cb6187f1c26f6330f9e6f07b34418af0c321742b
vnenise <rhjin886@naver.com>
2016-12-06 11:42:13 +0900

    hash함수 추가. (datetime+salt값 조합 => sha256)

---
+++ Util/hash.py
@@ -0,0 +1,39 @@
+import hashlib
+import uuid
+import datetime
+import string
+import random
+
+salt = "test"
+
+#datetime + salt => sha256
+def random_string1(salt, number):
+    base = str(datetime.datetime.now())
+    print "base: {0} salt: {1}".format(base, salt)
+    msg = base+", "+salt
+    print msg
+    hash = hashlib.sha256()
+    hash.update(base+", "+salt)
+
+    return hash.hexdigest()[:number]
+
+# #upper/lower/number => random chice
+# def random_string2(length):
+#     pool = string.letters + string.digits
+#     print pool
+#     return ''.join(random.choice(pool) for i in xrange(length))
+
+#uuid+salt => sha256
+# def random_string3(salt, number):
+#     base = str(uuid.uuid4().get_hex())
+#     print "base: {0} salt: {1}".format(base, salt)
+#     msg = base+", "+salt
+#     print msg
+#     hash = hashlib.sha256()
+#     hash.update(base+", "+salt)
+#
+#     return hash.hexdigest()[:number]
+
+# if __name__ == "__main__":
+    #     for i in range(100):
+    #         print(random_string1(salt,8))
