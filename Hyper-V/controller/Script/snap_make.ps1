3b86137a4f629a1a9a9c68f033286a8056476e6e
asdhgfasd <asdhgfasd@naver.com>
2016-12-08 10:19:10 +0900

    파워쉘 스크립트 업로드

---
+++ Hyper-V/controller/Script/snap_make.ps1
@@ -0,0 +1,8 @@
+﻿$VMname=$args[0];
+$CloneVMname=$args[1];
+$vm= Get-VM $VMname;
+$vmid = $vm.Name;
+Export-VM -Name $VMname -Path C:\images\clone\;
+Copy-Item C:\images\clone\$VMname\"Virtual Hard Disks"\*.vhdx -Destination C:\images\vhdx\$CloneVMname.vhdx;
+#임시저장소삭제
+Remove-Item -Path C:\images\clone -Recurse;
