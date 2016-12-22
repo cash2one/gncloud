8bbb99d42a1bfb919da8961bd2852a0caaa300c8
uphiller <jason.uphiller@gmail.com>
2016-12-22 11:24:45 +0900

    데쉬보드 추가

--- static/assets/js/angular/controllers/dashboard.js
+++ static/assets/js/angular/controllers/dashboard.js
@@ -17,10 +17,26 @@ angular
             .success(function (data, status, headers, config) {
                 if (data.status == true) {
                     new Chart(document.getElementById("cpu_chart").getContext("2d"), $scope.getConfig(data.list.cpu_per, "quota"));
-                    new Chart(document.getElementById("memory_chart").getContext("2d"), $scope.getConfig(data.list.memory_per, "quota"));
+                    new Chart(document.getElementById("mem_chart").getContext("2d"), $scope.getConfig(data.list.mem_per, "quota"));
                     new Chart(document.getElementById("disk_chart").getContext("2d"), $scope.getConfig(data.list.disk_per, "quota"));
                     new Chart(document.getElementById("status_chart").getContext("2d"), $scope.getConfig(data.list.vm_count, "status"));
                     new Chart(document.getElementById("type_chart").getContext("2d"), $scope.getConfig(data.list.vm_type, "type"));
+                    $scope.team_name = data.list.team_name;
+                    $scope.cpu_use_per = data.list.cpu_per[0];
+                    $scope.cpu_use_cnt = data.list.cpu_cnt[0];
+                    $scope.cpu_tot_cnt = data.list.cpu_cnt[1];
+                    $scope.mem_use_per = data.list.mem_per[0];
+                    $scope.mem_use_cnt = data.list.mem_cnt[0];
+                    $scope.mem_tot_cnt = data.list.mem_cnt[1];
+                    $scope.disk_use_per = data.list.disk_per[0];
+                    $scope.disk_use_cnt = data.list.disk_cnt[0];
+                    $scope.disk_tot_cnt = data.list.disk_cnt[1];
+                    $scope.docker_cnt = data.list.docker_info;
+                    $scope.hyperv_cnt = data.list.vm_type[1];
+                    $scope.kvm_cnt = data.list.vm_type[0];
+                    $scope.run_cnt = data.list.vm_count[0];
+                    $scope.stop_cnt = data.list.vm_count[1];
+                    $scope.tot_cnt = data.list.vm_count[0] + data.list.vm_count[1];
                 }
                 else {
                     alert(data.message)
@@ -34,13 +50,13 @@ angular
             var config = null;
             if (type == 'quota') {
                 config = {
-                    type: 'pie',
+                    type: 'doughnut',
                     data: {
                         datasets: [{
                             data: data,
                             backgroundColor: [
                                 "rgb(233, 30, 99)",
-                                "rgb(255, 193, 7)"
+                                "rgb(204, 204, 204)"
                             ],
                         }],
                         labels: [
@@ -55,7 +71,7 @@ angular
                 }
             }else if (type == 'status') {
                 config = {
-                    type: 'pie',
+                    type: 'doughnut',
                     data: {
                         datasets: [{
                             data: data,
@@ -76,7 +92,7 @@ angular
                 }
             }else if (type == 'type') {
                 config = {
-                    type: 'pie',
+                    type: 'doughnut',
                     data: {
                         datasets: [{
                             data: data,
