f4f8727032a2d008b3f3326c98fc41e6d400a07c
uphiller <jason.uphiller@gmail.com>
2016-12-26 10:33:43 +0900

    스크립트 nas 경로 수정

--- kvm/script/initcloud/sshkey_copy.sh
+++ kvm/script/initcloud/sshkey_copy.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-genisoimage -output /var/lib/libvirt/initcloud/config.iso -volid cidata -joliet -rock /var/lib/libvirt/initcloud/user-data /var/lib/libvirt/initcloud/meta-data
+genisoimage -output /data/kvm/initcloud/config.iso -volid cidata -joliet -rock /data/kvm/initcloud/user-data /data/kvm/initcloud/meta-data
