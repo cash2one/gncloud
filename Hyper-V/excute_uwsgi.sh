0cc9d45bb2fc63dc0c2fb1f1ce7be1bf46a384ba
uphiller <jason.uphiller@gmail.com>
2016-12-29 13:45:02 +0900

    쉘스크립트 수정

--- Hyper-V/excute_uwsgi.sh
+++ Hyper-V/excute_uwsgi.sh
@@ -1,1 +1,1 @@
-nohup uwsgi --http-socket :8083 --plugin python --wsgi-file __init__.py --logto  docker.log --callable app &
+nohup uwsgi --http-socket :8082 --plugin python --wsgi-file __init__.py --logto  hyperv.log --callable app &
