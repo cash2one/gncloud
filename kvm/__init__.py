38e4a6ee5d5b6d3b76793f9c62b392c6bb4df6fd
uphiller <jason.uphiller@gmail.com>
2017-01-09 15:01:31 +0900

    모니터링 인터벌 1분으로 조정

--- kvm/__init__.py
+++ kvm/__init__.py
@@ -128,7 +128,7 @@ def test():
 if __name__ == '__main__':
     #로그 설정
     cron = Scheduler(daemon=True)
-    cron.add_interval_job(job_function, seconds=180) #minites=1)
+    cron.add_interval_job(job_function, seconds=60)
     cron.start()
     app.run(port=8081)
     # http_server = WSGIServer(('', 8081), app)
