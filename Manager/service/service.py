addb4b0242aab1b98f0eb20154c9abe374dc3070
jshyoung90 <jshyoung90@naver.com>
2017-01-11 16:46:04 +0900

    docker 인스턴스 생성 수정

--- Manager/service/service.py
+++ Manager/service/service.py
@@ -83,11 +83,16 @@ def server_create(name, cpu, memory, disk, image_id, team_code, user_id, sshkeys
                               , type=type, team_code=team_code, author_id=user_id
                               , status='Starting', tag=tag, image_id=image_id, create_time=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
                               , host_id=host_id, hyperv_pass=password)
-    else:
+    elif(type=="kvm"):
         vm_machine = GnVmMachines(id=id, name=name, cpu=cpu, memory=memory, disk=disk
                                   , type=type, team_code=team_code, author_id=user_id
                                   , status='Starting', tag=tag, image_id=image_id, create_time=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
                                   , host_id="", ssh_key_id=sshkeys)
+    else:
+        vm_machine = GnVmMachines(id=id, name=name, cpu=cpu, memory=memory, disk=disk
+                                  , type=type, team_code=team_code, author_id=user_id
+                                  , status='Starting', tag=tag, image_id=image_id, create_time=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+                                  , host_id="")
                                 
     sql_session.add(vm_machine)
     sql_session.commit()
