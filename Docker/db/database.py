caa297aa3a0181c8ca4dd7be8f1cdc87184b9b79
FreshBread <white_len@naver.com>
2017-01-16 06:08:41 +0900

    Docker Controller import 일부 잘못된 경로 수정

--- Docker/db/database.py
+++ Docker/db/database.py
@@ -4,7 +4,7 @@ __author__ = 'jhjeon'
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from util.config import config
+from Docker.util.config import config
 
 
 engine = create_engine(config.DB_URL, convert_unicode=True)
