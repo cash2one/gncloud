54747387244788e3db25580ce3adb7ff7725e315
chonamhyun <erobeta@gmail.com>
2017-01-17 17:24:45 +0900

    update gn_users

--- install/gncloud-platform-db-registry/GN_USERS_superuser.sql
+++ install/gncloud-platform-db-registry/GN_USERS_superuser.sql
@@ -1,3 +1,3 @@
 connect gncloud;
-INSERT INTO `GN_USERS` (`user_id`, `password`, `user_name`, `privilege`, `tel`, `email`, `start_date`, `end_date`) VALUES ('root', password('12345678'), 'Root', NULL, '-', '-', '2017-01-06 12:24:23', NULL);
+INSERT INTO `GN_USERS` (`user_id`, `password`, `user_name`, `privilege`, `tel`, `email`, `start_date`, `end_date`) VALUES ('root', password('12345678'), 'Root', NULL, '-', '-', '2017-01-16 12:24:23', NULL);
 
