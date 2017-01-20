0a77ab3bb5736919f5a9f99d57621c425e4c3e12
uphiller <jason.uphiller@gmail.com>
2017-01-20 19:09:54 +0900

    kvm 스냅샷관련 로직 수정

--- Manager/service/service.py
+++ Manager/service/service.py
@@ -154,10 +154,13 @@ def server_change_status(id, status, sql_session):
         return True
 
 def vm_list(sql_session, team_code):
-    list = sql_session.query(GnVmMachines)\
-                      .filter(GnVmMachines.status != config.REMOVE_STATUS)\
-                      .filter(GnVmMachines.team_code == team_code)\
-                      .order_by(GnVmMachines.create_time.desc()).all()
+    list_query = sql_session.query(GnVmMachines) \
+                            .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                            .order_by(GnVmMachines.create_time.desc())
+    if team_code != "000":
+        list_query.filter(GnVmMachines.team_code == team_code)
+    list = list_query.all()
+
     for vmMachine in list:
         vmMachine.create_time = vmMachine.create_time.strftime('%Y-%m-%d %H:%M:%S')
         vmMachine.disk = convertHumanFriend(vmMachine.disk)
@@ -319,7 +322,12 @@ def server_image(type, sql_session, team_code):
     if type == "base":
         list = sql_session.query(GnVmImages).filter(GnVmImages.sub_type == type).filter(GnVmImages.status != config.REMOVE_STATUS).order_by(GnVmImages.create_time.desc()).all();
     else:
-        list = sql_session.query(GnVmImages).filter(GnVmImages.sub_type == type).filter(GnVmImages.team_code == team_code).filter(GnVmImages.status != config.REMOVE_STATUS).all()
+        list_query = sql_session.query(GnVmImages)\
+                                .filter(GnVmImages.sub_type == type)\
+                                .filter(GnVmImages.status != config.REMOVE_STATUS)
+        if team_code != "000":
+          list_query = list_query.filter(GnVmImages.team_code == team_code)
+        list = list_query.all()
 
     for vm in list:
         vm.create_time = vm.create_time.strftime('%Y-%m-%d %H:%M:%S')
@@ -331,85 +339,136 @@ def server_image(type, sql_session, team_code):
     return {"guest_list":list,"retryCheck":retryCheck}
 
 def getQuotaOfTeam(team_code, sql_session):
-    current_info = sql_session.query(func.sum(GnVmMachines.cpu).label("sum_cpu"),
-                                     func.sum(GnVmMachines.memory).label("sum_mem"))\
-                              .filter(GnVmMachines.team_code == team_code)\
-                              .filter(GnVmMachines.status != config.REMOVE_STATUS).filter(GnVmMachines.status != config.ERROR_STATUS).one()
 
-    current_disk_info = sql_session.query(func.sum(GnVmMachines.disk).label("sum_disk"))\
-                                   .filter(GnVmMachines.team_code == team_code)\
-                                   .filter(GnVmMachines.status != config.REMOVE_STATUS) \
-                                   .filter(GnVmMachines.type != "docker").filter(GnVmMachines.status != config.ERROR_STATUS).one()
-
-    limit_quota = sql_session.query(GnTeam)\
-                             .filter(GnTeam.team_code == team_code).one()
-
-    vm_run_count = sql_session.query(func.count(GnVmMachines.id).label("count"))\
-                              .filter(GnVmMachines.team_code == team_code)\
-                              .filter(GnVmMachines.status == config.RUN_STATUS) \
-                              .filter(GnVmMachines.type != "docker").filter(GnVmMachines.status != config.ERROR_STATUS).one()
-
-    vm_stop_count = sql_session.query(func.count(GnVmMachines.id).label("count")) \
-                               .filter(GnVmMachines.team_code == team_code)\
+    #현재 cpu, 메모리 사용량
+    current_query = sql_session.query(func.sum(GnVmMachines.cpu).label("sum_cpu"),
+                                      func.sum(GnVmMachines.memory).label("sum_mem")) \
                                .filter(GnVmMachines.status != config.REMOVE_STATUS)\
-                               .filter(GnVmMachines.status != config.RUN_STATUS)\
-                               .filter(GnVmMachines.type != "docker").filter(GnVmMachines.status != config.ERROR_STATUS).one()
+                               .filter(GnVmMachines.status != config.ERROR_STATUS)
+    if team_code != "000":
+        current_query = current_query.filter(GnVmMachines.team_code == team_code)
+    current_info = current_query.one()
 
-    vm_kvm_count = sql_session.query(func.count(GnVmMachines.id).label("count")) \
-                              .filter(GnVmMachines.team_code == team_code)\
-                              .filter(GnVmMachines.status != config.REMOVE_STATUS)\
-                              .filter(GnVmMachines.type == "kvm").filter(GnVmMachines.status != config.ERROR_STATUS).one()
+    #현재 disk 사용량
+    current_disk_query = sql_session.query(func.sum(GnVmMachines.disk).label("sum_disk")) \
+                                    .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                                    .filter(GnVmMachines.type != "docker")
+    if team_code != "000":
+        current_disk_query = current_disk_query.filter(GnVmMachines.team_code == team_code)
+    current_disk_info = current_disk_query.one()
 
-    vm_hyperv_count = sql_session.query(func.count(GnVmMachines.id).label("count")) \
-                                 .filter(GnVmMachines.team_code == team_code)\
-                                 .filter(GnVmMachines.status != config.REMOVE_STATUS)\
-                                 .filter(GnVmMachines.type == "hyperv").filter(GnVmMachines.status != config.ERROR_STATUS).one()
+    #쿼터 제한
+    if team_code != "000":
+        limit_quota = sql_session.query(GnTeam)\
+                                 .filter(GnTeam.team_code == team_code).one()
+    else:
+        limit_quota = sql_session.query(func.sum(GnTeam.cpu_quota).label("cpu_quota"),
+                                        func.sum(GnTeam.mem_quota).label("mem_quota"),
+                                        func.sum(GnTeam.disk_quota).label("disk_quota")).one()
 
-    vm_docker_count = sql_session.query(func.count(GnVmMachines.id).label("count")) \
-                                  .filter(GnVmMachines.team_code == team_code)\
-                                  .filter(GnVmMachines.status != config.REMOVE_STATUS)\
-                                  .filter(GnVmMachines.type == "docker").filter(GnVmMachines.status != config.ERROR_STATUS).one()
+    #러닝 인스턴스
+    vm_run_query = sql_session.query(func.count(GnVmMachines.id).label("count")) \
+                              .filter(GnVmMachines.status == config.RUN_STATUS) \
+                              .filter(GnVmMachines.type != "docker")\
+                              .filter(GnVmMachines.status != config.ERROR_STATUS)
+    if team_code != "000":
+        vm_run_query = vm_run_query.filter(GnVmMachines.team_code == team_code)
+    vm_run_count = vm_run_query.one()
 
+    #정지 인스턴스
+    vm_stop_query = sql_session.query(func.count(GnVmMachines.id).label("count")) \
+                               .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                               .filter(GnVmMachines.status != config.RUN_STATUS) \
+                               .filter(GnVmMachines.type != "docker")\
+                               .filter(GnVmMachines.status != config.ERROR_STATUS)
+    if team_code != "000":
+        vm_stop_query = vm_stop_query.filter(GnVmMachines.team_code == team_code)
+    vm_stop_count = vm_stop_query.one()
+
+    #kvm 인스턴스 개수
+    vm_kvm_query = sql_session.query(func.count(GnVmMachines.id).label("count")) \
+                              .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                              .filter(GnVmMachines.type == "kvm") \
+                              .filter(GnVmMachines.status != config.ERROR_STATUS)
+    if team_code != "000":
+        vm_kvm_query = vm_kvm_query.filter(GnVmMachines.team_code == team_code)
+    vm_kvm_count = vm_kvm_query.one()
+
+
+    #hyperv 인스턴스 개수
+    vm_hyperv_query = sql_session.query(func.count(GnVmMachines.id).label("count")) \
+                                 .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                                 .filter(GnVmMachines.type == "hyperv") \
+                                 .filter(GnVmMachines.status != config.ERROR_STATUS)
+    if team_code != "000":
+        vm_hyperv_query = vm_hyperv_query.filter(GnVmMachines.team_code == team_code)
+    vm_hyperv_count = vm_hyperv_query.one()
+
+
+    #docker 인스턴스 개수
+    vm_docker_query = sql_session.query(func.count(GnVmMachines.id).label("count")) \
+                                 .filter(GnVmMachines.team_code == team_code) \
+                                 .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                                 .filter(GnVmMachines.type == "docker") \
+                                 .filter(GnVmMachines.status != config.ERROR_STATUS)
+    if team_code != "000":
+        vm_docker_query = vm_docker_query.filter(GnVmMachines.team_code == team_code)
+    vm_docker_count = vm_docker_query.one()
+
+    #팀정보
     team_info = sql_session.query(GnTeam)\
                            .filter(GnTeam.team_code == team_code).one()
 
-    team_user_cnt = sql_session.query(func.count(GnUserTeam.user_id).label("count"))\
-                               .filter(GnUserTeam.team_code == team_code)\
-                               .filter(GnUserTeam.comfirm == "Y").one()
+    #팀유저
+    team_user_query = sql_session.query(func.count(GnUserTeam.user_id).label("count")) \
+                                 .filter(GnUserTeam.comfirm == "Y")
+    if team_code != "000":
+        team_user_query = team_user_query.filter(GnUserTeam.team_code == team_code)
+    team_user_cnt = team_user_query.one()
 
-    user_list = sql_session.query(GnVmMachines.author_id,GnUser.user_name,func.count().label("count"))\
-                           .outerjoin(GnUser, GnVmMachines.author_id == GnUser.user_id)\
-                           .filter(GnVmMachines.team_code == team_code)\
-                           .filter(GnVmMachines.status != config.REMOVE_STATUS) \
-                           .filter(GnVmMachines.type != "docker").filter(GnVmMachines.status != config.ERROR_STATUS)\
-                           .group_by(GnVmMachines.author_id).all()
+    #유저별 vm리스트
+    user_list_query = sql_session.query(GnVmMachines.author_id,GnUser.user_name,func.count().label("count")) \
+                                 .outerjoin(GnUser, GnVmMachines.author_id == GnUser.user_id) \
+                                 .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                                 .filter(GnVmMachines.type != "docker")\
+                                 .filter(GnVmMachines.status != config.ERROR_STATUS) \
+                                 .group_by(GnVmMachines.author_id)\
+                                 .order_by(func.count().desc())
+    if team_code != "000":
+        user_list_query = user_list_query.filter(GnVmMachines.team_code == team_code)
+    user_list = user_list_query.all()
 
-    image_type_list = sql_session.query(GnVmImages.name, func.count().label("count")) \
-                                 .join(GnVmMachines,  GnVmImages.id == GnVmMachines.image_id) \
-                                 .filter(GnVmMachines.status != config.REMOVE_STATUS).filter(GnVmMachines.status != config.ERROR_STATUS) \
-                                 .filter(GnVmMachines.team_code == team_code) \
-                                 .group_by(GnVmImages.id).all()
+    #이미지별 vm리스트
+    image_type_query = sql_session.query(GnVmImages.name, func.count().label("count")) \
+                                  .join(GnVmMachines, GnVmImages.id == GnVmMachines.image_id) \
+                                  .filter(GnVmMachines.status != config.REMOVE_STATUS) \
+                                  .filter(GnVmMachines.status != config.ERROR_STATUS) \
+                                  .group_by(GnVmImages.id) \
+                                  .order_by(func.count().desc())
+    if team_code != "000":
+        image_type_query = image_type_query.filter(GnVmMachines.team_code == team_code)
+    image_type_list = image_type_query.all()
 
     if current_info.sum_cpu is None:
         cpu_per_info = [0,100]
-        cpu_cnt_info = [0,limit_quota.cpu_quota]
+        cpu_cnt_info = [0,int(limit_quota.cpu_quota)]
     else:
-        cpu_per_info = [int((current_info.sum_cpu/limit_quota.cpu_quota)*100), 100 - (int((current_info.sum_cpu/limit_quota.cpu_quota)*100))]
-        cpu_cnt_info = [int(current_info.sum_cpu), limit_quota.cpu_quota]
+        cpu_per_info = [int((current_info.sum_cpu/int(limit_quota.cpu_quota))*100), 100 - (int((current_info.sum_cpu/int(limit_quota.cpu_quota))*100))]
+        cpu_cnt_info = [int(current_info.sum_cpu), int(limit_quota.cpu_quota)]
 
     if current_info.sum_mem is None:
         memory_per_info = [0,100]
-        mem_cnt_info = [0, convertHumanFriend(limit_quota.mem_quota)]
+        mem_cnt_info = [0, convertHumanFriend(int(limit_quota.mem_quota))]
     else:
-        memory_per_info = [int((current_info.sum_mem/limit_quota.mem_quota)*100), 100 - (int((current_info.sum_mem/limit_quota.mem_quota)*100))]
-        mem_cnt_info = [convertHumanFriend(int(current_info.sum_mem)), convertHumanFriend(limit_quota.mem_quota)]
+        memory_per_info = [int((current_info.sum_mem/int(limit_quota.mem_quota))*100), 100 - (int((current_info.sum_mem/int(limit_quota.mem_quota))*100))]
+        mem_cnt_info = [convertHumanFriend(int(current_info.sum_mem)), convertHumanFriend(int(limit_quota.mem_quota))]
 
     if current_disk_info.sum_disk is None:
         disk_per_info = [0,100]
-        disk_cnt_info = [0, convertHumanFriend(limit_quota.disk_quota)]
+        disk_cnt_info = [0, convertHumanFriend(int(limit_quota.disk_quota))]
     else:
-        disk_per_info = [int((current_disk_info.sum_disk/limit_quota.disk_quota)*100), 100 - (int((current_disk_info.sum_disk/limit_quota.disk_quota)*100))]
-        disk_cnt_info = [convertHumanFriend(int(current_disk_info.sum_disk)), convertHumanFriend(limit_quota.disk_quota)]
+        disk_per_info = [int((current_disk_info.sum_disk/int(limit_quota.disk_quota))*100), 100 - (int((current_disk_info.sum_disk/int(limit_quota.disk_quota))*100))]
+        disk_cnt_info = [convertHumanFriend(int(current_disk_info.sum_disk)), convertHumanFriend(int(limit_quota.disk_quota))]
 
     count_info = [vm_run_count.count,vm_stop_count.count]
     type_info = [vm_kvm_count.count,vm_hyperv_count.count]
@@ -420,8 +479,6 @@ def getQuotaOfTeam(team_code, sql_session):
         vm_kvm_per = (vm_kvm_count.count*100)/(vm_kvm_count.count+vm_hyperv_count.count)
     if vm_hyperv_count.count != 0:
         vm_hyperv_per = (vm_hyperv_count.count*100)/(vm_kvm_count.count+vm_hyperv_count.count)
-
-
 
     quato_info = {'team_name':team_info.team_name, 'cpu_per':cpu_per_info, 'mem_per':memory_per_info, 'disk_per':disk_per_info
                  , 'cpu_cnt':cpu_cnt_info, 'mem_cnt':mem_cnt_info, 'disk_cnt':disk_cnt_info
