cd54ca3b9668ee181b68f7114be8e2f98243b7d0
chonamhyun <erobeta@gmail.com>
2017-01-25 16:31:23 +0900

    new scheduler

--- scheduler/util/hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import datetime
-
-def random_string(salt, number):
-    base = str(datetime.datetime.now())
-    hash = hashlib.sha256()
-    hash.update(base+", "+salt)
-    return hash.hexdigest()[:number]
