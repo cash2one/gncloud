8abffffaa709a2732fcfb61ffca68f4079edcd33
chonamhyun <erobeta@gmail.com>
2017-01-25 16:53:26 +0900

    seperate url name

--- install/gncloud-platform-controller/source-install.sh
+++ install/gncloud-platform-controller/source-install.sh
@@ -12,7 +12,9 @@ cp /usr/local/source/gncloud/sites-available/nginx.conf /etc/nginx/.
 systemctl restart nginx
 
 # controller 들 start
-#cd /usr/local/source/gncloud; nohup uwsgi --http-socket :8080 --plugin python --wsgi-file ./Manager/__init__.py --logto  ./Manager/manager.log --callable app &
+#cd /usr/local/source/gncloud/Docker; nohup uwsgi --http-socket :8083 --plugin python --wsgi-file __init__.py  --logto docker.log --callable app &
+
+#cd /usr/local/source/gncloud; nohup uwsgi --http-socket :8083 --plugin python --wsgi-file ./Docker/__init__.py  --logto ./Docker/docker.log --callable app &
 #cd /usr/local/source/gncloud; nohup uwsgi --http-socket :8083 --plugin python --wsgi-file ./Docker/__init__.py --logto  ./Docker/docker.log --callable app &
 #cd /usr/local/source/gncloud; nohup uwsgi --http-socket :8081 --plugin python --wsgi-file ./kvm/__init__.py --logto  ./kvm/kvm.log --callable app &
 #cd /usr/local/source/gncloud; nohup uwsgi --http-socket :8082 --plugin python --wsgi-file ./HyperV/__init__.py --logto  ./HyperV/hyperv.log --callable app &
