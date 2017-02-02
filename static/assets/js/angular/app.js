b74e0903b799b290670df991a8e8e42f4fc59812
uphiller <jason.uphiller@gmail.com>
2017-02-02 14:38:50 +0900

    관리자 시스템 설정 화면 우측 메뉴 공통 파일로 추출

--- static/assets/js/angular/app.js
+++ static/assets/js/angular/app.js
@@ -6,20 +6,35 @@ var serviceAddModules = [
 
 (function () {
 
-    var app = angular.module('gncloud', serviceAddModules)
-        .directive('navbar', function (){
-            return{
-                restrict: 'E',
-                templateUrl:'/main/navbar.html',
-                controller:'navbarCtrl'
+    var app = angular.module('gncloud', serviceAddModules);
+
+    app.directive('navbar', function (){
+        return{
+            restrict: 'E',
+            templateUrl:'/main/navbar.html',
+            controller: function ($scope, $location) {
+
             }
-        }).directive('navleft', function (){
-            return{
-                restrict: 'E',
-                templateUrl:'/main/navleft.html',
-                controller:'navleftCtrl'
+        }
+    });
+
+    app.directive('navleft', function (){
+        return{
+            restrict: 'E',
+            templateUrl:'/main/navleft.html',
+            controller: function ($scope, $location) {
+                if($location.path() == "/guestSystemList") $scope.menu = 1;
+                if($location.path() == "/guestTeamList") $scope.menu = 2;
+                if($location.path() == "/guestCluster") $scope.menu = 3;
+                if($location.path() == "/guestImage") $scope.menu = 4;
+                if($location.path() == "/guestPrice") $scope.menu = 5;
+                if($location.path() == "/guestLoginHist") $scope.menu = 6;
+                if($location.path() == "/guestUseHist") $scope.menu = 7;
+                if($location.path() == "/guestInvoice") $scope.menu = 8;
+                if($location.path() == "/guestSetting") $scope.menu = 9;
             }
-        });
+        };
+    });
 
     app.run(function($rootScope,$http){
         $http({
