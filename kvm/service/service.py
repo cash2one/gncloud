7876933ad30e0808d1ed35f4dbaab486536064c6
uphiller <jason.uphiller@gmail.com>
2017-02-07 15:41:19 +0900

    kvm 생성로직 수정

--- kvm/service/service.py
+++ kvm/service/service.py
@@ -33,15 +33,10 @@ def server_create(team_code, user_id, user_name, id, sql_session):
         print("complete init vm!!!")
 
         #ip 세팅
-        s = pxssh.pxssh()
-        s.login(host_info.ip, USER)
-
         ip = ""
         while len(ip) == 0:
             print(id+":processing init ip!!!")
-            s.sendline(config.SCRIPT_PATH+"get_ipaddress.sh " + internal_name)
-            ip = s.before.replace(config.SCRIPT_PATH+"get_ipaddress.sh " + internal_name + "\r\n", "")
-        s.logout()
+            ip = getIpAddress(internal_name, host_info.ip)
         print("complete get ip="+ip)
 
         # 기존 저장된 ssh key 등록
@@ -93,18 +88,15 @@ def setSsh(host_ip, path, ip, ssh_id):
         print("setSsh:"+e)
         pass
 
-# def getIpAddress(name, host_ip):
-#     try:
-#         s = pxssh.pxssh()
-#         s.login(host_ip, USER)
-#         s.sendline(config.SCRIPT_PATH+"get_ipaddress.sh " + name)
-#         s.prompt()
-#         ip = s.before.replace(config.SCRIPT_PATH+"get_ipaddress.sh " + name + "\r\n", "")
-#         s.logout()
-#         return ip
-#     except IOError as e:
-#         print("getIpAddress:"+e)
-#         pass
+def getIpAddress(name, host_ip):
+    s = pxssh.pxssh()
+    s.login(host_ip, USER)
+    s.sendline(config.SCRIPT_PATH+"get_ipaddress.sh " + name)
+    s.prompt()
+    ip = s.before.split("\r\n", 1)[1]
+    s.logout()
+    return ip
+
 
 
 def setStaticIpAddress(ip, host_ip, ssh_id):
