8823bbbe9423fb1b0ad28eaa31621fe50f411309
jshyoung90 <jshyoung90@naver.com>
2017-02-09 15:47:24 +0900

    notifiy 수정

--- static/assets/js/angular/controllers/guestSnapList.js
+++ static/assets/js/angular/controllers/guestSnapList.js
@@ -1,6 +1,6 @@
 angular
     .module('gncloud')
-    .controller('guestSnapListCtrl', function ($scope, $http, dateModifyService, $timeout, $interval, $rootScope) {
+    .controller('guestSnapListCtrl', function ($scope, $http, dateModifyService, $timeout, $interval, $rootScope, notification) {
         $scope.user_info = $rootScope.user_info;
         $rootScope.$on('init', function () {
             $scope.user_info = $rootScope.user_info;
