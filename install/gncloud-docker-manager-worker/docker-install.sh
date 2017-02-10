d1f364356a1f17695c42303d0c2e778826cc7ec8
chonamhyun <erobeta@gmail.com>
2017-02-10 18:13:32 +0900

    swarm init

--- install/gncloud-docker-manager-worker/docker-install.sh
+++ install/gncloud-docker-manager-worker/docker-install.sh
@@ -20,3 +20,9 @@ sed -i "s/ExecStart=\/usr\/bin\/dockerd/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\
 systemctl enable docker
 systemctl start docker
 
+# docker manager
+# docker swarm init --advertise-addr [IP]
+# result token : SWMTKN-1-49nj1cmql0jkz5s954yi3oex3nedyz0fb0xx14ie39trti4wxv-8vxv8rssmk743ojnwacrr2e7c
+# docker workers
+# docker swarm join --token SWMTKN-1-49nj1cmql0jkz5s954yi3oex3nedyz0fb0xx14ie39trti4wxv-8vxv8rssmk743ojnwacrr2e7c [IP]:2377
+
