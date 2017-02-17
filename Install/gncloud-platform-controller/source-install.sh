fd380aee5ca001edaec00785e1d15f0f5ace681a
CHONAMHYUN <erobeta@gmail.com>
2017-02-17 10:31:33 +0900

    실행파일 위치 디렉토리 변경에 따른 수정

--- Install/gncloud-platform-controller/source-install.sh
+++ Install/gncloud-platform-controller/source-install.sh
@@ -14,7 +14,7 @@ mkdir -p /var/log/gncloud
 export PYTHONPATH=/var/lib/gncloud
 # controller 들 start
 nohup uwsgi --http-socket :8080 --plugin python --wsgi-file /var/lib/gncloud/Manager/__init__.py --logto  /var/log/gncloud/manager_controller.log --processes 4 --threads 2 --callable app &
-nohup uwsgi --http-socket :8081 --plugin python --wsgi-file /var/lib/gncloud/kvm/__init__.py --logto /var/log/gncloud/kvm_controller.log  --processes 4 --threads 2 --callable app &
+nohup uwsgi --http-socket :8081 --plugin python --wsgi-file /var/lib/gncloud/KVM/__init__.py --logto /var/log/gncloud/kvm_controller.log  --processes 4 --threads 2 --callable app &
 nohup uwsgi --http-socket :8082 --plugin python --wsgi-file /var/lib/gncloud/HyperV/__init__.py --logto /var/log/gncloud/hyperv_controller.log  --processes 4 --threads 2 --callable app &
 nohup uwsgi --http-socket :8083 --plugin python --wsgi-file /var/lib/gncloud/Docker/__init__.py --logto /var/log/gncloud/docker_controller.log  --processes 4 --threads 2 --callable app &
-nohup uwsgi --http-socket :8084 --plugin python --wsgi-file /var/lib/gncloud/scheduler/__init__.py --logto /var/log/scheduler.log --callable app --enable-threads &
+nohup uwsgi --http-socket :8084 --plugin python --wsgi-file /var/lib/gncloud/Scheduler/__init__.py --logto /var/log/scheduler.log --callable app --enable-threads &
