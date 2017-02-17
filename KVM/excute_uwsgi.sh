ff3660404e87426f818ea72d7ce334bc6a95dbcb
jshyoung90 <jshyoung90@naver.com>
2017-02-17 17:05:27 +0900

    Dockerfile 수정

--- KVM/excute_uwsgi.sh
+++ KVM/excute_uwsgi.sh
@@ -1,1 +1,2 @@
-nohup uwsgi --http-socket :8081 --plugin python --wsgi-file __init__.py --processes 4 --threads 2 --logto kvm.log --callable app &
+#!/bin/bash
+uwsgi --http-socket :$1 --plugin python --wsgi-file /var/lib/gncloud/KVM/__init__.py --logto /var/log/gncloud/kvm.log --processes 4 --threads 2 --callable app
--- Manager/excute_uwsgi.sh
+++ Manager/excute_uwsgi.sh
@@ -1,1 +1,2 @@
-nohup uwsgi --http-socket :8080 --plugin python --wsgi-file __init__.py --logto  manager.log --processes 4 --threads 2 --callable app &
+#!/bin/bash
+uwsgi --http-socket :$1 --plugin python --wsgi-file /var/lib/gncloud/Manager/__init__.py --logto /var/log/gncloud/manager.log --processes 4 --threads 2 --callable app
