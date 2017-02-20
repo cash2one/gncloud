6abd39b716dfb0bcc830a047019df940e5b258fa
jshyoung90 <jshyoung90@naver.com>
2017-02-20 11:33:05 +0900

    execute파일 수정

--- Manager/execute_uwsgi.sh
+++ Manager/execute_uwsgi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-uwsgi --http-
-nohup socket :8080 --plugin python --wsgi-file /var/lib/gncloud/Manager/__init__.py --logto /var/log/gncloud/manager.log --processes 4 --threads 2 --callable app &
+
+nohup uwsgi --http-socket :8080 --plugin python --wsgi-file /var/lib/gncloud/Manager/__init__.py --logto /var/log/gncloud/manager.log --processes 4 --threads 2 --callable app &
 
