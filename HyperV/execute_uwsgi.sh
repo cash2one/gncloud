e2e92986035d4ac4d5df87d080d3456b9000dd7a
CHONAMHYUN <erobeta@gmail.com>
2017-02-20 18:30:44 +0900

    centos로 BASE 변경 test

--- HyperV/execute_uwsgi.sh
+++ HyperV/execute_uwsgi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup uwsgi --http-socket :8082 --plugin python --wsgi-file /var/lib/gncloud/HyperV/__init__.py --logto /var/log/gncloud/hyperv.log --processes 4 --threads 2 --callable app &
-# uwsgi --http-socket :80 --plugin python --wsgi-file /var/lib/gncloud/HyperV/__init__.py --logto /var/log/gncloud/hyperv.log --processes 4 --threads 2 --callable app
+# nohup uwsgi --http-socket :8082 --plugin python --wsgi-file /var/lib/gncloud/HyperV/__init__.py --logto /var/log/gncloud/hyperv.log --processes 4 --threads 2 --callable app &
+uwsgi --http-socket :80 --plugin python --wsgi-file /var/lib/gncloud/HyperV/__init__.py --logto /var/log/gncloud/hyperv.log --processes 4 --threads 2 --callable app
