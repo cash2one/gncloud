6ed0fcf94f61306fb15d4480dfddc7aed634792a
CHONAMHYUN <erobeta@gmail.com>
2017-02-20 18:34:17 +0900

    centos로 BASE 변경 test

--- Docker/execute_uwsgi.sh
+++ Docker/execute_uwsgi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# nohup uwsgi --http-socket :8083 --plugin python --wsgi-file /var/lib/gncloud/Docker/__init__.py --logto /var/log/gncloud/docker.log --processes 4 --threads 2 --callable app &
-uwsgi --http-socket :80 --plugin python --wsgi-file /var/lib/gncloud/Docker/__init__.py --logto /var/log/gncloud/docker.log --processes 4 --threads 2 --callable app
+nohup uwsgi --http-socket :8083 --plugin python --wsgi-file /var/lib/gncloud/Docker/__init__.py --logto /var/log/gncloud/docker.log --processes 4 --threads 2 --callable app &
+# uwsgi --http-socket :80 --plugin python --wsgi-file /var/lib/gncloud/Docker/__init__.py --logto /var/log/gncloud/docker.log --processes 4 --threads 2 --callable app
