b997d659e25f370815b51744d2a56e2aa0af7a98
jshyoung90 <jshyoung90@naver.com>
2017-02-20 11:29:18 +0900

    execute파일 수정

--- Docker/execute_uwsgi.sh
+++ Docker/execute_uwsgi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup uwsgi --http-socket :$1 --plugin python --wsgi-file /var/lib/gncloud/Docker/__init__.py --logto /var/log/gncloud/docker.log --processes 4 --threads 2 --callable app &
+nohup uwsgi --http-socket :8083 --plugin python --wsgi-file /var/lib/gncloud/Docker/__init__.py --logto /var/log/gncloud/docker.log --processes 4 --threads 2 --callable app &
--- Scheduler/execute_uwsgi.sh
+++ Scheduler/execute_uwsgi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uwsgi --http-socket :$1 --plugin python --wsgi-file /var/lib/gncloud/Scheduler/__init__.py --logto /var/log/gncloud/scheduler.log --callable app
+nohup uwsgi --http-socket :8084 --plugin python --wsgi-file /var/lib/gncloud/Scheduler/__init__.py --logto /var/log/gncloud/scheduler.log --callable app &
