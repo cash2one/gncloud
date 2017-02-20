2a3e0388da798d589f8d7db83693cd573a309063
CHONAMHYUN <erobeta@gmail.com>
2017-02-20 15:30:43 +0900

    docker 내부 포트는 80으로 세팅

--- KVM/execute_uwsgi.sh
+++ KVM/execute_uwsgi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nohup uwsgi --http-socket :8081 --plugin python --wsgi-file /var/lib/gncloud/KVM/__init__.py --logto /var/log/gncloud/kvm.log --processes 4 --threads 2 --callable app &
-# uwsgi --http-socket :$1 --plugin python --wsgi-file /var/lib/gncloud/KVM/__init__.py --logto /var/log/gncloud/kvm.log --processes 4 --threads 2 --callable app
+# uwsgi --http-socket :80 --plugin python --wsgi-file /var/lib/gncloud/KVM/__init__.py --logto /var/log/gncloud/kvm.log --processes 4 --threads 2 --callable app
