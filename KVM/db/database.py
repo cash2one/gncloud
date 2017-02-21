1078aac2f53043d2bc3a0fc820c501c9f764079d
jshyoung90 <jshyoung90@naver.com>
2017-02-21 16:01:44 +0900

    에러 수정

--- KVM/db/database.py
+++ KVM/db/database.py
@@ -6,20 +6,9 @@ from KVM.util.config import config
 
 import os
 
-ip=os.environ['IP']
-user=os.environ['USER']
-pswd=os.environ['PASS']
-database_name=os.environ['DBNAME']
-port=os.environ['PORT']
 
-if port != '':
-    port = ':%s' % port
-if ip != '':
-    DB_URL = 'mysql://' + user + ':' + pswd + '@' + ip + port + '/' + database_name + '?charset=utf8'
-else:
-    DB_URL = config.DB_URL
 
-engine = create_engine(DB_URL, convert_unicode=True)
+engine = create_engine(config.DB_URL, convert_unicode=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
--- Manager/db/database.py
+++ Manager/db/database.py
@@ -8,20 +8,9 @@ from Manager.util.config import config
 
 import os
 
-ip=os.environ['IP']
-user=os.environ['USER']
-pswd=os.environ['PASS']
-database_name=os.environ['DBNAME']
-port=os.environ['PORT']
 
-if port != '':
-    port = ':%s' % port
-if ip != '':
-    DB_URL = 'mysql://' + user + ':' + pswd + '@' + ip + port + '/' + database_name + '?charset=utf8'
-else:
-    DB_URL = config.DB_URL
 
-engine = create_engine(DB_URL, convert_unicode=True, pool_recycle=500, pool_size=5, max_overflow=20, echo=False,
+engine = create_engine(config.DB_URL, convert_unicode=True, pool_recycle=500, pool_size=5, max_overflow=20, echo=False,
                        echo_pool=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
