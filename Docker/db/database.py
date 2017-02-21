9919c716be6fb4cc03bfb60ced69fb18228b885e
CHONAMHYUN <erobeta@gmail.com>
2017-02-21 13:39:35 +0900

    docker compose 용 DB connect 수정

--- Docker/db/database.py
+++ Docker/db/database.py
@@ -7,7 +7,22 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from Docker.util.config import config
 
-engine = create_engine(config.DB_URL, convert_unicode=True)
+import os
+
+ip=os.environ['IP']
+user=os.environ['USER']
+pswd=os.environ['PASS']
+database_name=os.environ['DBNAME']
+port=os.environ['PORT']
+
+if port != '':
+    port = ':%s' % port
+if ip != '':
+    DB_URL = 'mysql://' + user + ':' + pswd + '@' + ip + port + '/' + database_name + '?charset=utf8'
+else:
+    DB_URL = config.DB_URL
+
+engine = create_engine(DB_URL, convert_unicode=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
