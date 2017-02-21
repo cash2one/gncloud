ad62a5b619db9bd23d9b3d2ce9ec77893ceef083
CHONAMHYUN <erobeta@gmail.com>
2017-02-21 14:03:06 +0900

    docker compose test

--- HyperV/db/database.py
+++ HyperV/db/database.py
@@ -6,8 +6,22 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from HyperV.util.config import config
+import os
 
-engine = create_engine(config.DB_URL, convert_unicode=True)
+ip=os.environ['IP']
+user=os.environ['USER']
+pswd=os.environ['PASS']
+database_name=os.environ['DBNAME']
+port=os.environ['PORT']
+
+if port != '':
+    port = ':%s' % port
+if ip != '':
+    DB_URL = 'mysql://' + user + ':' + pswd + '@' + ip + port + '/' + database_name + '?charset=utf8'
+else:
+    DB_URL = config.DB_URL
+
+engine = create_engine(DB_URL, convert_unicode=True)
 
 # engine = create_engine(config.DB_URL, convert_unicode=True, pool_recycle=500, pool_size=5, max_overflow=20, echo=False,
 #                        echo_pool=True, extend_existing=True)
