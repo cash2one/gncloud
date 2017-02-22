eb68b30720a2a38b934ac7d3e90a212beb779c5f
jshyoung90 <jshyoung90@naver.com>
2017-02-22 16:10:34 +0900

    생성 삭제

--- KVM/service/service.py
+++ KVM/service/service.py
@@ -85,7 +85,7 @@ def setSsh(host_ip, pub,org,name, ip, ssh_id):
         print(":processing set sshkey!!!")
         s = pxssh.pxssh(timeout=1200)
         s.login(host_ip, USER)
-        s.sendline(config.SCRIPT_PATH+"make_sshkeys.sh '"+ name+"' " + str(org)+" " + str(pub))
+        s.sendline(config.SCRIPT_PATH+"make_sshkeys.sh "+ name+" '" + str(org)+"' '" + str(pub)+"'")
         s.sendline(config.SCRIPT_PATH+"add_sshkeys.sh '"+name + "' " + str(ip) + " "+ssh_id)
         s.logout()
         print(":complete set sshkey!!!")
