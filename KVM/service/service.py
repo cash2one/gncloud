189bed70ca391d46d1b90646d11cde586c5742fe
주성훈 <shjoo@juseonghunui-MacBook-Pro.local>
2017-02-22 14:40:32 +0900

    생성 삭제

--- KVM/service/service.py
+++ KVM/service/service.py
@@ -90,7 +90,7 @@ def setSsh(host_ip, pub,org,name, ip, ssh_id):
         print(":processing set sshkey!!!")
         s = pxssh.pxssh(timeout=1200)
         s.login(host_ip, USER)
-        s.sendline(config.SCRIPT_PATH+"add_sshkeys.sh '" +"/tmp/"+name + "' " + str(ip) + " "+ssh_id)
+        s.sendline(config.SCRIPT_PATH+"add_sshkeys.sh '" +config.SSHKEY_PATH+name + "' " + str(ip) + " "+ssh_id)
         s.logout()
         print(":complete set sshkey!!!")
     except IOError as e:
