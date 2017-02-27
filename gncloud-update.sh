a4e6164911d8698eb01e14dd917768d28e98b611
CHONAMHYUN <erobeta@gmail.com>
2017-02-27 15:47:11 +0900

    gncloud update from gncloud in docker hub

---
+++ gncloud-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker login
+docker pull gncloud/gncloud-docker
+docker pull gncloud/gncloud-web
+docker pull gncloud/gncloud-manager
+docker pull gncloud/gncloud-scheduler
+docker pull gncloud/gncloud-hyperv
+docker pull gncloud/gncloud-kvm
+docker pull gncloud/gncloud-mysql
+docker pull gncloud/gncloud-registry
